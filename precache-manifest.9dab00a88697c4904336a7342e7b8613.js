25d0254d9247c3ae2e1aee393564d41afb5242e1
sandipsaha597 <40121135+sandipsaha597@users.noreply.github.com>
2019-06-14 15:29:02 +0530

    Add files via upload

---
+++ precache-manifest.9dab00a88697c4904336a7342e7b8613.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54bb7d42010899eac13cd36aac0f1305",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "4b5ae50a6adce1b21dc1",
+    "url": "/my-portfolio/static/css/main.a3ced49c.chunk.css"
+  },
+  {
+    "revision": "75f15be0ebd6de7babdb",
+    "url": "/my-portfolio/static/js/2.1857569f.chunk.js"
+  },
+  {
+    "revision": "4b5ae50a6adce1b21dc1",
+    "url": "/my-portfolio/static/js/main.2b2ad973.chunk.js"
+  },
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  },
+  {
+    "revision": "8bd0426fd105af7e0b0546122cb71983",
+    "url": "/my-portfolio/static/media/bars.8bd0426f.JPG"
+  },
+  {
+    "revision": "17a51ab745ed1da366a2890b3b9bd072",
+    "url": "/my-portfolio/static/media/book3D.17a51ab7.JPG"
+  },
+  {
+    "revision": "337f481c294c1b275046a613504575ad",
+    "url": "/my-portfolio/static/media/customAnimations.337f481c.JPG"
+  },
+  {
+    "revision": "18dc44269d9c1eb1e7fed6e0a1a11dab",
+    "url": "/my-portfolio/static/media/giveAway.18dc4426.JPG"
+  },
+  {
+    "revision": "1f47ae77dcb9a02d5f609dc5445358e7",
+    "url": "/my-portfolio/static/media/newsSite.1f47ae77.JPG"
+  },
+  {
+    "revision": "98d203cef34a024143e9e8c7e71b30f1",
+    "url": "/my-portfolio/static/media/popUp.98d203ce.JPG"
+  },
+  {
+    "revision": "70b617154ae07b97d1fc4dc12e7333be",
+    "url": "/my-portfolio/static/media/profile.70b61715.JPG"
+  },
+  {
+    "revision": "bfd673f4b3d7460e44a91b39af418b19",
+    "url": "/my-portfolio/static/media/renovation.bfd673f4.JPG"
+  },
+  {
+    "revision": "1fa5239e30986c39e0c2d80aeb4a765f",
+    "url": "/my-portfolio/static/media/self.1fa5239e.JPG"
+  },
+  {
+    "revision": "a8615aa53fb0d6ce46bb35bef70629a8",
+    "url": "/my-portfolio/static/media/weatherApp.a8615aa5.JPG"
+  }
+]);
